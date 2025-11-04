f3f183d904741a73dc8ce97884b77f0f3d7bc726
mk-pmb <mk-pmb@users.noreply.github.com>
2025-11-04 01:20:56 +0100

    Increase the default memory ulimit.

--- ulimited.sh
+++ ulimited.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # -*- coding: utf-8, tab-width: 2 -*-
-ulimit -v $(( ${ESLINT_MAX_MEMORY_MB:-512} * 1024 ))
+ulimit -v $(( ${ESLINT_MAX_MEMORY_MB:-1024} * 1024 ))
+# 2025-11-04: Granting "mere" 512 MB caused malloc errors even for tiny files.
+
 SELFPATH="$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")" # busybox
 BEST_NODEJS="$(which node{js,.js,} |& grep -m 1 -Pe '^/')"
 exec "$BEST_NODEJS" "$SELFPATH"/elp.js "$@"; exit $?
